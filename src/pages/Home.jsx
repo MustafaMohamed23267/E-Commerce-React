c77216de71d33700374bfe0475fe02ddf561b818
Mustafa Mohamed <120753752+MustafaMohamed23267@users.noreply.github.com>
2026-01-12 18:29:45 +0200

    update mobile home width fit 3

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -154,9 +154,9 @@ export default function Home()
     return(
     <>
     <Nav/>
-    <main className="max-sm:w-full">
-        <section className=" flex flex-col max-sm:w-screen">
-        <img src={commerce} className=" w-full max-sm:w-full h-screen z-0" />
+    <main >
+        <section className=" flex flex-col">
+        <img src={commerce} className=" w-full max-sm:w-fit h-screen z-0" />
         <div className="absolute z-10 pt-40 max-sm:pt-20 pl-10 max-sm:w-60">
             <h2 className="text-[#003d29] text-7xl font-bold max-sm:text-4xl">shopping and <br/>
                 department store.</h2>
