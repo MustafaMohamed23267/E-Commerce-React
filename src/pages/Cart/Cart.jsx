b82461d4abd7eeb0f288bc0576eda60b1b0110c6
Mustafa Mohamed <120753752+MustafaMohamed23267@users.noreply.github.com>
2026-07-12 00:31:27 +0300

    update cart page 2

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -16,11 +16,35 @@ export default function Cart()
                 <hr className="text-gray-300"/>
             </div>
 
-            <div className="px-10 pt-5">
-                <section >
+            <div className=" flex px-10 pt-5 space-x-4">
+                <section className="space-y-5 w-[65%]">
                     <h2 className="text-[#003d29] text-4xl">
                         Your Cart
                     </h2>
+
+                    <table className="w-full px-6">
+                        <thead className="border-b border-gray-300">
+                            <tr className="flex space-x-16 text-[#003d29] font-semibold pb-2">
+                                <td className="w-80">products</td>
+                                <td>Price</td>
+                                <td className="w-30">Quantity</td>
+                                <td>total</td>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                            <tr>
+                                <td>image</td>
+                                <td></td>
+                            </tr>
+                        </tbody>
+
+                    </table>
+                </section>
+
+
+                <section className="space-y-5 w-[30%] h-90 rounded-xl bg-[#003d29]">
+
                 </section>
             </div>
             
