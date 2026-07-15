9b965fabc73b367d143e4d7dae67b44ac7a697e1
Mustafa Mohamed <120753752+MustafaMohamed23267@users.noreply.github.com>
2026-07-15 10:49:50 +0300

    update store

--- src/Context/store.js
+++ src/Context/store.js
@@ -1,11 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from '../pages/Login/AuthSlice';
 
+import productsReducer from '../pages/Products/ProductSlice'
+
 
 export const Store = configureStore(
     {
         reducer:{
-            auth:authReducer
+            auth:authReducer,
+            products:productsReducer
         }
     }
 )
